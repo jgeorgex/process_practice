dfe9b614fff7b258a64a00444201f95b216d9d80
Julian George <jules104@gmail.com>
2019-12-03 09:32:20 +0000

    Tests still pass for two letter words

--- middle_letter/spec/get_middle_spec.rb
+++ middle_letter/spec/get_middle_spec.rb
@@ -8,4 +8,8 @@ describe "#get_middle" do
     it "returns A when given A" do
         expect(get_middle('A')).to eq 'A'
     end
+
+    it "returns bb when given bb" do
+        expect(get_middle('bb')).to eq 'bb'
+    end
 end
