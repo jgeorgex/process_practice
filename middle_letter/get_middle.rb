f19e334f98334cca722897bf7bcfe6c7f790f737
Julian George <jules104@gmail.com>
2019-12-03 10:20:18 +0000

    Tests passing for all acceptance criteria provided

--- middle_letter/get_middle.rb
+++ middle_letter/get_middle.rb
@@ -5,6 +5,8 @@ def get_middle(word)
         letter = word.slice(word.length/2)
         letter
     else
-        "bc"
+        letter_one = word.slice((word.length/2)-1)
+        letter_two = word.slice(word.length/2)
+        return "#{letter_one}#{letter_two}"
     end
 end
--- middle_letter/spec/get_middle_spec.rb
+++ middle_letter/spec/get_middle_spec.rb
@@ -20,4 +20,26 @@ describe "#get_middle" do
     it "returns bc when given abcd" do
         expect(get_middle('abcd')).to eq 'bc'
     end
+
+    it "returns cd when given abcdef" do
+        expect(get_middle('abcdef')).to eq 'cd'
+    end
+
+    it "returns of when given of" do
+        expect(get_middle('of')).to eq 'of'
+    end
+
+    it "returns t when given testing" do
+        expect(get_middle('testing')).to eq 't'
+    end
+
+    it "returns es when given test" do
+        expect(get_middle('test')).to eq 'es'
+    end
+
+    it "returns dd when given middle" do
+        expect(get_middle('middle')).to eq 'dd'
+    end
+
+
 end
